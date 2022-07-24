58cfef940bf88208141f5014fcf3172401e74bba
arturjpaulo <70978076+arturjpaulo@users.noreply.github.com>
2022-07-24 12:08:41 -0700

    Update mask_register.sh

--- mask_register.sh
+++ mask_register.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 # This script registers a ROI file (in std space) to the native space (diffusion).
-# Requirements: DWI file, lesion mask (same DWI file name as a prefix) and you ROI (filename ROI)
+# Requirements: DWI file, std_mask (same DWI file name as a prefix) and you ROI (filename ROI)
 for originalFile in *[^_lesion_mask][^_ROI].nii;
     do
          echo Running "$originalFile"... ;
         
         # Defining file names...
         afflineFile=${originalFile%.nii}_affline_transf.mat;
-        lesionMaskFile=${originalFile%.nii}_lesion_mask;
+        stdMaskFile=${originalFile%.nii}_lesion_mask;
         warpedFAFile2MNI=${originalFile%.nii}_my_warped_fa;
         roiFile=ROI;
         nonlinear_transf=${originalFile%.nii}_my_nonlinear_transf;
@@ -18,7 +18,7 @@ for originalFile in *[^_lesion_mask][^_ROI].nii;
         flirt -ref ${FSLDIR}/data/standard/FMRIB58_FA_1mm -in ${originalFile} -omat ${afflineFile};
         echo Flirt ok!
 
-        fnirt --in=${originalFile} --aff=${afflineFile} --cout=${nonlinear_transf} --inmask=${lesionMaskFile} --config=FA_2_FMRIB58_1mm.cnf;
+        fnirt --in=${originalFile} --aff=${afflineFile} --cout=${nonlinear_transf} --inmask=${stdMaskFile} --config=FA_2_FMRIB58_1mm.cnf;
         echo Fnirt ok!
 
         applywarp --ref=${FSLDIR}/data/standard/FMRIB58_FA_1mm --in=${originalFile} --warp=${nonlinear_transf} --out=${warpedFAFile2MNI};
@@ -30,6 +30,5 @@ for originalFile in *[^_lesion_mask][^_ROI].nii;
         applywarp --ref=${originalFile} --in=${roiFile} --warp=${MNI2DTI} --out=${roi_DTIspace} --interp=nn;
         echo Applywarp MNI2DTI ok!
 
-        fslstats ${originalFile}.nii.gz -k ${roi_DTIspace} -M -S >> ${originalFile%.nii}_${roiFile}.txt;
-        echo fslstats ok!;
+       
 done
